f59df720e359a14bc7e7b6893aa6cff9d23362e5
TorsteinEide <77608436+TorsteinEide@users.noreply.github.com>
2024-12-12 15:25:08 +0100

    Revert "feat: (frontend) finished layout page"
    This reverts commit ed7b53e9089261b033f8d820cc8294590d107085.

--- frontend/src/app/components/tabbar.tsx
+++ frontend/src/app/components/tabbar.tsx
@@ -7,30 +7,22 @@ import BottomNavigationAction from '@mui/material/BottomNavigationAction';
 import CalendarMonthIcon from '@mui/icons-material/CalendarMonth';
 import CakeIcon from '@mui/icons-material/Cake';
 import TipsAndUpdatesIcon from '@mui/icons-material/TipsAndUpdates';
-import {useContext} from 'react';
-import PageContext from '../context/PageContext';  
 
 const Tabbar: React.FC = () => {
   const [value, setValue] = React.useState(0);
-  const { setCurrentPage } = useContext(PageContext);  
-
-  const pageMapping = ["Events", "Idébank", "Kakefredag"];
-
-    const handleTabChange = (event: React.SyntheticEvent, newValue: number) => {
-        setValue(newValue);
-        setCurrentPage(pageMapping[newValue]);  // Update currentPage based on selected tab
-    };
 
   return (
-    <Box >
-      <BottomNavigation sx={{ backgroundColor: '#202020'  }}
+    <Box>
+      <BottomNavigation
         showLabels
         value={value}
-        onChange={handleTabChange}  // Use the new function here
+        onChange={(event, newValue) => {
+          setValue(newValue);
+        }}
       >
-        <BottomNavigationAction sx={{color: '#FFFFFF', paddingTop: '5px'}} className="h-fit" label="Events" icon={<CalendarMonthIcon />} />
-        <BottomNavigationAction sx={{color: '#FFFFFF', paddingTop: '5px'}} className="h-fit" label="Idébank" icon={<TipsAndUpdatesIcon />} />
-        <BottomNavigationAction sx={{color: '#FFFFFF', paddingTop: '5px'}} className="h-fit" label="Kakefredag" icon={<CakeIcon />} />
+        <BottomNavigationAction label="Events" icon={<CalendarMonthIcon />} />
+        <BottomNavigationAction label="Idébank" icon={<TipsAndUpdatesIcon />} />
+        <BottomNavigationAction label="Kakefredag" icon={<CakeIcon />} />
       </BottomNavigation>
     </Box>
   );
--- frontend/src/app/page.tsx
+++ frontend/src/app/page.tsx
@@ -1,17 +1,9 @@
-'use client'
-import React, { useState } from 'react';
 import Tabbar from "./components/tabbar";
-import PageLayout from "./components/PageLayout";
-import PageContext from './context/PageContext';  // Adjust the import path
 
 export default function Home() {
-  const [currentPage, setCurrentPage] = useState("Events");
-
-  return (
-    <PageContext.Provider value={{ currentPage, setCurrentPage }}>
-      <PageLayout title={currentPage}>
-        {//Child pages}
-      </PageLayout>
-    </PageContext.Provider>
+  return(
+    <main>
+      <Tabbar></Tabbar>
+    </main>
   );
 }
