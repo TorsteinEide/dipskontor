0798d668ba20110d6f71f41106eeaf64c5777f8f
Stig Rune Smedås <srs@dips.no>
2024-12-12 16:22:08 +0100

    Add-button in Event page

--- frontend/src/app/pages/events.tsx
+++ frontend/src/app/pages/events.tsx
@@ -2,13 +2,25 @@
 
 import * as React from 'react';
 import Box from '@mui/material/Box';
+import { PulsAlert, PulsButton } from "@dips/puls-core-react-components";
+import { PlusIcon } from "@dips/arena-core-icons";
 
 const Events: React.FC = () => {
 
+  const showAlert = false;
+  function addEvent() {
+    // Show alert for now
+    //throw new Error('Sorry, budsjettet er oppbrukt!!');
+  }
+
   return (
     <Box>
-        kjempe
+      No e vi i event-sida!<br/>
+      <PulsButton mode="secondary" size="medium" onClick = {addEvent()} >
+        <PlusIcon />Legg til event
+      </PulsButton>
     </Box>
   );
 }
+
 export default Events;
