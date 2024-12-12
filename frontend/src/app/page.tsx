d4c6d0c50b8333824e09a0d8b648eb19dea64bd0
eisv <eisv@dips.no>
2024-12-12 15:21:08 +0100

    Added routing

--- frontend/src/app/page.tsx
+++ frontend/src/app/page.tsx
@@ -1,9 +1,18 @@
+'use client';
+
 import Tabbar from "./components/tabbar";
+import { BrowserRouter, Routes, Route, HashRouter } from "react-router-dom";
+import Events from "./pages/events";
+import Ideabank from "./pages/ideabank";
 
 export default function Home() {
   return(
-    <main>
-      <Tabbar></Tabbar>
-    </main>
+    <HashRouter>
+      <Routes>
+            <Route index element={<Events />} />
+            <Route path="ideabank" element={<Ideabank />} />
+      </Routes>
+      <Tabbar/>
+    </HashRouter>
   );
 }
