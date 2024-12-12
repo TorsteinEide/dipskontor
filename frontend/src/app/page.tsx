727b7a1bdf8559c0903db078030d1c72afde0f22
TorsteinEide <77608436+TorsteinEide@users.noreply.github.com>
2024-12-12 15:26:05 +0100

    Reapply "feat: (frontend) finished layout page"
    This reverts commit f59df720e359a14bc7e7b6893aa6cff9d23362e5.

--- frontend/src/app/page.tsx
+++ frontend/src/app/page.tsx
@@ -1,9 +1,17 @@
+'use client'
+import React, { useState } from 'react';
 import Tabbar from "./components/tabbar";
+import PageLayout from "./components/PageLayout";
+import PageContext from './context/PageContext';  // Adjust the import path
 
 export default function Home() {
-  return(
-    <main>
-      <Tabbar></Tabbar>
-    </main>
+  const [currentPage, setCurrentPage] = useState("Events");
+
+  return (
+    <PageContext.Provider value={{ currentPage, setCurrentPage }}>
+      <PageLayout title={currentPage}>
+        {//Child pages}
+      </PageLayout>
+    </PageContext.Provider>
   );
 }
