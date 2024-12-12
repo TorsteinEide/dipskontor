670194da0f92b1db217958aec1f4a9cb80a1f649
TorsteinEide <77608436+TorsteinEide@users.noreply.github.com>
2024-12-12 16:07:39 +0100

    fix: (frontend) delted package-lock

--- frontend/src/app/components/PageLayout.tsx
+++ frontend/src/app/components/PageLayout.tsx
@@ -13,11 +13,9 @@ const PageLayout: React.FC<PageLayoutProps> = ({ title, children }) => {
       <header className="w-full py-4 text-center">
         <h1 className="lg:text-3xl text-4xl">{title}</h1>
       </header>
-      
       <main className="flex-1 overflow-y-auto px-4 py-6">
         {children}
        </main>
-      
       <Tabbar className="bg-gray-800 text-white w-full py-4 fixed bottom-0 left-0" />
     </div>
   );
