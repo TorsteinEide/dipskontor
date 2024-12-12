5ac70049d36e37ebbc3e8880d962e07e68bed318
Magnus Alsaker <magnusalsaker@Magnus-MacBook-DIPS.local>
2024-12-12 13:20:31 +0100

    added tabbar components

--- dipskontor-frontend/src/app/page.tsx
+++ dipskontor-frontend/src/app/page.tsx
@@ -1,7 +1,9 @@
-import Image from "next/image";
+import Tabbar from "./components/tabbar";
 
 export default function Home() {
   return(
-    <div>Hello world</div> 
+    <main>
+      <Tabbar></Tabbar>
+    </main>
   );
 }
