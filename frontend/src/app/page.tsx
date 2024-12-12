a480eb8c1baff1d4238f48d6aea707f603f66f03
Stig Rune Smedås <srs@dips.no>
2024-12-12 15:44:08 +0100

    Add event button

--- frontend/src/app/page.tsx
+++ frontend/src/app/page.tsx
@@ -1,9 +1,15 @@
+import { PulsButton, PulsIconButton } from "@dips/puls-core-react-components";
+import { PlusIcon } from "@dips/arena-core-icons";
 import Tabbar from "./components/tabbar";
 
 export default function Home() {
   return(
     <main>
       <Tabbar></Tabbar>
+      <PulsButton mode="primary" size="medium">
+        <PlusIcon />Legg til event
+      </PulsButton>
+
     </main>
   );
 }
