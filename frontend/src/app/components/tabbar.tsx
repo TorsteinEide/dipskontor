ed7b53e9089261b033f8d820cc8294590d107085
TorsteinEide <taurstein@gmail.com>
2024-12-12 15:20:44 +0100

    feat: (frontend) finished layout page

--- frontend/src/app/components/tabbar.tsx
+++ frontend/src/app/components/tabbar.tsx
@@ -7,22 +7,30 @@ import BottomNavigationAction from '@mui/material/BottomNavigationAction';
 import CalendarMonthIcon from '@mui/icons-material/CalendarMonth';
 import CakeIcon from '@mui/icons-material/Cake';
 import TipsAndUpdatesIcon from '@mui/icons-material/TipsAndUpdates';
+import {useContext} from 'react';
+import PageContext from '../context/PageContext';  
 
 const Tabbar: React.FC = () => {
   const [value, setValue] = React.useState(0);
+  const { setCurrentPage } = useContext(PageContext);  
+
+  const pageMapping = ["Events", "Idébank", "Kakefredag"];
+
+    const handleTabChange = (event: React.SyntheticEvent, newValue: number) => {
+        setValue(newValue);
+        setCurrentPage(pageMapping[newValue]);  // Update currentPage based on selected tab
+    };
 
   return (
-    <Box>
-      <BottomNavigation
+    <Box >
+      <BottomNavigation sx={{ backgroundColor: '#202020'  }}
         showLabels
         value={value}
-        onChange={(event, newValue) => {
-          setValue(newValue);
-        }}
+        onChange={handleTabChange}  // Use the new function here
       >
-        <BottomNavigationAction label="Events" icon={<CalendarMonthIcon />} />
-        <BottomNavigationAction label="Idébank" icon={<TipsAndUpdatesIcon />} />
-        <BottomNavigationAction label="Kakefredag" icon={<CakeIcon />} />
+        <BottomNavigationAction sx={{color: '#FFFFFF', paddingTop: '5px'}} className="h-fit" label="Events" icon={<CalendarMonthIcon />} />
+        <BottomNavigationAction sx={{color: '#FFFFFF', paddingTop: '5px'}} className="h-fit" label="Idébank" icon={<TipsAndUpdatesIcon />} />
+        <BottomNavigationAction sx={{color: '#FFFFFF', paddingTop: '5px'}} className="h-fit" label="Kakefredag" icon={<CakeIcon />} />
       </BottomNavigation>
     </Box>
   );
