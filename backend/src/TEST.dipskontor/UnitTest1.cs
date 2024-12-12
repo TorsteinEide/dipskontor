2e65b58f0673aab2c5f0af1e681dbeec0b4b6e33
Fredrik Hagen Fasteraune <frha@dips.no>
2024-12-12 13:40:45 +0100

    feat: Add test project and some sqls

---
+++ backend/src/TEST.dipskontor/UnitTest1.cs
@@ -0,0 +1,10 @@
+﻿namespace test.dipskontor;
+
+public class UnitTest1
+{
+    [Fact]
+    public void Test1()
+    {
+
+    }
+}
---
+++ backend/src/dipskontor/Events/CreateEvent.sql
@@ -0,0 +1,1 @@
+INSERT INTO Events (title, description, created_at, location) VALUES (:Title, :Description, :CreatedAt, :Location);
