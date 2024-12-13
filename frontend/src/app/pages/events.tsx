dde10ebec648a78a07014df8282dd96a1201a70e
Stig Rune Smedås <srs@dips.no>
2024-12-13 09:24:38 +0100

    wip: add temp action to event

--- frontend/src/app/pages/events.tsx
+++ frontend/src/app/pages/events.tsx
@@ -7,18 +7,32 @@ import { PlusIcon } from "@dips/arena-core-icons";
 
 const Events: React.FC = () => {
 
-  const showAlert = false;
-  function addEvent() {
-    // Show alert for now
-    //throw new Error('Sorry, budsjettet er oppbrukt!!');
-  }
+    // Declare a state variable named "toggle" with an initial value of false
+    const [toggle, setAlert] = React.useState(false);
+  
+    // Function to toggle the value of the "toggle" state
+    const toggleAlert = () => {
+      setAlert(!toggle);
+    };
 
   return (
     <Box>
       No e vi i event-sida!<br/>
-      <PulsButton mode="secondary" size="medium" onClick = {addEvent()} >
+      
+      <PulsButton mode="secondary" size="medium" 
+        onClick={toggleAlert}>
         <PlusIcon />Legg til event
       </PulsButton>
+
+      { toggle ? 
+      <PulsAlert
+        type="error"
+        format="messageBox"
+        title="Å nei du!"
+        onClose={toggleAlert}>
+          <p>Budsjettet for all moro er oppbrukt for i år!</p>
+      </PulsAlert> : '' }
+
     </Box>
   );
 }
